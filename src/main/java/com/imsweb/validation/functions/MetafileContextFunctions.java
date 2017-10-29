472b503fad255316c974aabd15acd210d0d629bd
depryf <depryf@imsweb.com>
2017-10-29 16:44:51 -0400

    Trying to fix trimming issue...

--- src/main/java/com/imsweb/validation/functions/MetafileContextFunctions.java
+++ src/main/java/com/imsweb/validation/functions/MetafileContextFunctions.java
@@ -1028,7 +1028,7 @@ public class MetafileContextFunctions extends StagingContextFunctions {
             return false;
 
         // value searched for is trimmed...
-        int idx = index.find(trimRight(val));
+        int idx = index.find(val);
 
         // side effect, fill in any requested tableVar
         if (tableVars != null && table != null) {
@@ -1138,7 +1138,7 @@ public class MetafileContextFunctions extends StagingContextFunctions {
             return false;
 
         // value searched for is trimmed...
-        int idx = index.findFloor(trimRight(val));
+        int idx = index.findFloor(val);
 
         // side effect, fill in any requested tableVar
         if (tableVars != null && table != null) {
