9f9a9a1ca52c41d6d0fc085022b0614b58657f00
depryf <depryf@imsweb.com>
2018-09-13 11:45:39 -0400

    No need to increase the validation engine version!

--- src/main/java/com/imsweb/validation/ValidationEngine.java
+++ src/main/java/com/imsweb/validation/ValidationEngine.java
@@ -79,7 +79,7 @@ public final class ValidationEngine {
     /**
      * Engine version (used to check compatibility with the edits)
      */
-    private static final String _ENGINE_VERSION = "5.12";
+    private static final String _ENGINE_VERSION = "5.11";
 
     /**
      * The different context types supported by the engine
